2e1f1be19c71deba0db3361a42ef200b6d399dab
portiaportia <portiaplante@gmail.com>
2019-11-18 10:28:33 -0500

    adding Library Project structure

---
+++ Library/src/Items.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+
+public class Items {
+	private static Items items;
+	private ArrayList<Item> itemList;
+	
+	private Items() {
+		itemList = Database.getItems();
+	}
+	
+	public static Items getInstance() {
+		if(items == null) {
+			items = new Items();
+		}
+		
+		return items;
+	}
+
+	public boolean haveItem(String itemName) {
+		return true;
+	}
+	
+	public Item getItem(String itemName) {
+		return null;
+	}
+}
